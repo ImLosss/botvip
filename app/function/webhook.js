baf89f7bc0807b7a7fd23c0dae2fd47aef81c605
ImLosss <pahriansyahryan@gmail.com>
2026-06-19 00:21:10 +0800

    update payment

--- app/function/webhook.js
+++ app/function/webhook.js
@@ -38,6 +38,9 @@ module.exports = function(bot) {
               vipData[chatId].order_id = null;
               vipData[chatId].amount = null;
               vipData[chatId].qris_expiry = null;
+
+              bot.deleteMessage(chatId, vipData[chatId].message_id).catch(err => console.error('Failed to delete message:', err.message));
+              
               vipData[chatId].message_id = null;
 
               writeJSONFileSync('database/vip_users.json', vipData);
