98c4d7869f8cca78219c95d3039c098f128901ee
ImLosss <pahriansyahryan@gmail.com>
2026-06-19 23:03:51 +0800

    add help

--- app/function/help.js
+++ app/function/help.js
@@ -2,17 +2,17 @@ require('module-alias/register');
 const { readJSONFileSync, writeJSONFileSync } = require('function/utils');
 
 async function helpAdmin(bot, msg) {
-    const helpText = `🎬 **Series Commands**
-/series add <series_id> - Add a new series
-/series delete <series_id> - Delete a series
-/series update <series_id> - Update series information
-/series episode <series_id> - Add a new episode to a series
-/series delepisode <series_id> - Delete an episode from a series
-/series search <title> - Search for series by title
+    const helpText = `🎬 *Series Commands*
+\`/series add [series_id]\` - Add a new series
+\`/series delete [series_id]\` - Delete a series
+\`/series update [series_id]\` - Update series information
+\`/series episode [series_id]\` - Add a new episode to a series
+\`/series delepisode [series_id]\` - Delete an episode from a series
+\`/series search [title]\` - Search for series by title
 
-💎 **VIP & Akun**
-/vipcode - Generate a VIP code
-/claimvip <vip_code> - Claim a VIP code`
+💎 *VIP & Akun*
+\`/vipcode\` - Generate a VIP code
+\`/claimvip [vip_code]\` - Claim a VIP code`;
 
 return bot.sendMessage(msg.chat.id, helpText, { parse_mode: 'Markdown' });
 
