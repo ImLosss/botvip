16bb7be7e4086d33ba34c5292a7bc6e3f2a2439b
ImLosss <pahriansyahryan@gmail.com>
2026-09-13 21:07:37 +0800

    update

--- app/function/help.js
+++ app/function/help.js
@@ -13,7 +13,8 @@ async function helpAdmin(bot, msg) {
 💎 <b>VIP & Akun</b>
 <code>/vipcode</code> - Generate a VIP code
 <code>/claimvip [vip_code]</code> - Claim a VIP code
-<code>/setdisc</code> - Set discount for VIP users`;
+<code>/setdisc</code> - Set discount for VIP users
+<code>/setmonths</code> - Set allowed months for VIP purchase`;
 
     return bot.sendMessage(msg.chat.id, helpText, { parse_mode: 'HTML' });
 }
